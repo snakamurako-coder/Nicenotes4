16b323ee56618a27c5766fd6a15da47a447ad096
中村紘二郎 <snakamurako@yamagataps.jp>
2026-06-15 11:23:26 +0900

    複製時に引き継ぐ情報を選択可能。画像も対応。

--- コード.js
+++ コード.js
@@ -6,13 +6,14 @@ function doGet() {
 
 /**
  * ホーム画面の並び・フォルダ閲覧モード等（JSON を ScriptProperties に保存）。
- * @return {{ folderPdfMode: Object, folderKind: Object, fileLogicalPages: Object, layout: { childrenOrder: Object, fileOrder: Object, fileDue: Object } }}
+ * @return {{ folderPdfMode: Object, folderKind: Object, fileLogicalPages: Object, fileDupPagePrefs: Object, layout: { childrenOrder: Object, fileOrder: Object, fileDue: Object } }}
  */
 function nn_getWorkspaceMeta_() {
   const def = {
     folderPdfMode: {},
     folderKind: {},
     fileLogicalPages: {},
+    fileDupPagePrefs: {},
     layout: { childrenOrder: {}, fileOrder: {}, fileDue: {} },
   };
   try {
@@ -22,6 +23,7 @@ function nn_getWorkspaceMeta_() {
     if (!o.folderPdfMode || typeof o.folderPdfMode !== 'object') o.folderPdfMode = {};
     if (!o.folderKind || typeof o.folderKind !== 'object') o.folderKind = {};
     if (!o.fileLogicalPages || typeof o.fileLogicalPages !== 'object') o.fileLogicalPages = {};
+    if (!o.fileDupPagePrefs || typeof o.fileDupPagePrefs !== 'object') o.fileDupPagePrefs = {};
     if (!o.layout || typeof o.layout !== 'object') o.layout = {};
     if (!o.layout.childrenOrder) o.layout.childrenOrder = {};
     if (!o.layout.fileOrder) o.layout.fileOrder = {};
